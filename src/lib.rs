fee5fc59b82f017dd0427d36890b4fc043897b6a
liam <liam.wirth1@outlook.com>
2023-08-23 11:59:15 -0700

    consulted egui discord for help, this is before I actually get to work on using the advice they gave me

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,45 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
-pub use app::TemplateApp;
+pub use app::MyApp;
 pub mod array;
+```rust
+pub struct Array {
+    elements: Vec<Shape>,
+    element_width: f32,
+    element_gap: f32,
+    init: bool,
+}
+
+impl Array {
+    pub fn new(size: usize, ui: Ui) -> Self {
+        let mut rng = thread_rng();
+        let mut elements: Vec<Shape> = Vec::new();
+        let element_gap = 2.0;
+        let element_width = ui.available_width() / size as f32;
+        
+        let mut x = 0.0;
+        for _ in 0..size {
+            let height = rng.gen_range(1..=size) as f32 / size as f32 * ui.available_size().y;
+            let rect = egui::Rect::from_min_size(
+                Pos2::new(x, ui.available_size().y - height),
+                Vec2::new(element_width - 2.0, height),
+            );
+            let out = Shape::rect_filled(rect, 0.0, Color32::from_rgb(100, 100, 100));
+            elements.push(out);
+            x += element_width;
+        }
+        let init = true;
+        Array {
+            elements,
+            element_width,
+            element_gap,
+            init,
+        }
+    }
+//Other methods such as shuffle, is_sorted, swap, get_element, and remove, are also present, but I didnt paste them here because I don't feel they are relevant
+pub fn draw(&self, ui: &mut egui::Ui) {
+        ui.painter().extend(self.elements.clone());
+    }
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(sortingvisualizer::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(sortingvisualizer::MyApp::new(cc))),
     )
 }
 
