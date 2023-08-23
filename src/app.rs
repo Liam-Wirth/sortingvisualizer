b2fb839840297f04ad0a60472b79b426de0c740a
liam <liam.wirth1@outlook.com>
2023-08-23 15:31:20 -0700

    wait, I'm goated ; '

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,18 @@
 use egui::{Ui, Pos2, Color32, Shape};
 use eframe::{egui};
+use egui::Vec2;
 
-use crate::array::{self, Array};
+use crate::{array::{self, Array}};
 
 pub struct MyApp {
     label: String,
+    array: Array,
 }
 impl Default for MyApp {
     fn default() -> Self {
         Self {
             label: "SortingVisualizer".to_owned(),
+            array: Array::new(20),
         }
     }
 }
@@ -25,6 +28,11 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
             egui::warn_if_debug_build(ui);
+            if self.array.init {
+                let draw = convert_array( self.array.get_elements(), ui);
+                ui.painter().extend(draw.clone());
+                self.array.shuffle();
+            }
         });
 
         if false {
@@ -34,6 +42,23 @@ impl eframe::App for MyApp {
                 ui.label("You can turn on resizing and scrolling if you like.");
                 ui.label("You would normally choose either panels OR windows.");
             });
+            
+        }
+        fn convert_array(array: Vec<u32>, ui: &mut Ui) -> Vec<Shape> {
+            let mut x = 0.0;
+            let element_width = ui.available_width()/array.len() as f32;
+            let mut elements: Vec<Shape> = Vec::new();
+            for i in &array {
+               let height = *i as f32 / array.len() as f32 * ui.available_size().y;
+                let rect = egui::Rect::from_min_size(
+                   Pos2::new(x, ui.available_size().y - height),
+                    Vec2::new(element_width-2.0, height),
+                );
+                let out = Shape::rect_filled(rect, 0.0, Color32::from_rgb(100, 100, 100));
+                elements.push(out);
+                x += element_width;
+            }; 
+            elements
         }
     }
 }
--- src/array.rs
+++ src/array.rs
@@ -3,23 +3,25 @@ use rand;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use rand::Rng;
+
 //TODO: refactor this code so as to just handle f32s as the "height" variable, maybe better to even
 //normalize them to like be between 0-1 and then just multiply that out to whatever the size of the
 //window frame is? dunno
 pub struct Array {
-    elements: Vec<Shape>,
+    elements: Vec<u32>,
     element_width: f32,
     element_gap: f32,
-    init: bool,
+    pub init: bool,
 }
 
 impl Array {
+    /*
     pub fn new(size: usize, ui: Ui) -> Self {
         let mut rng = thread_rng();
         let mut elements: Vec<Shape> = Vec::new();
         let element_gap = 2.0;
         let element_width = ui.available_width() / size as f32;
-        
+
         let mut x = 0.0;
         for _ in 0..size {
             let height = rng.gen_range(1..=size) as f32 / size as f32 * ui.available_size().y;
@@ -39,24 +41,33 @@ impl Array {
             init,
         }
     }
+    */
+    pub fn new(size: usize) -> Self {
+        let mut rng = thread_rng();
+        let mut elements: Vec<u32> = Vec::new();
+        for i in (1..size) {
+            elements.push(i as u32);
+        }
+        elements.shuffle(&mut rng);
+        Array {
+            elements,
+            element_gap: 2.0,
+            element_width: 2.0,
+            init: true,
+        }
+
+    }
     pub fn shuffle(&mut self) {
         let mut rng = rand::thread_rng();
         self.elements.shuffle(&mut rng);
     }
     pub fn is_sorted(&self) -> bool {
-        for i in 0..self.elements.len() - 1 {
-            //NOTE: this is likely unsafe code LOLE!
-            let (rect1, rect2) = (&self.elements[i], &self.elements[i + 1]);
-            if rect1.visual_bounding_rect().height() > rect2.visual_bounding_rect().height() {
-                return false;
-            }
-        }
-        true
+        self.elements.windows(2).all(|w| w[0] <= w[1])
     }
     pub fn swap(&mut self, i: usize, j: usize) {
         self.elements.swap(i, j);
     }
-    pub fn get_element(&self, index: usize) -> Shape {
+    pub fn get_element(&self, index: usize) -> u32 {
         self.elements[index].clone()
     }
     pub fn len(&self) -> usize {
@@ -68,7 +79,12 @@ impl Array {
         }
         self.elements.remove(index);
     }
+    pub fn get_elements(&self) -> Vec<u32> {
+        self.elements.clone()
+    }
+    /*
     pub fn draw(&self, ui: &mut egui::Ui) {
         ui.painter().extend(self.elements.clone());
     }
+    */
 }
